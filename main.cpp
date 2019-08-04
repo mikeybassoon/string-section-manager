0a77775d03a81b799a6b0b19579bb3c6ae39d1c5
Mike Macaulay <49072733+mikeybassoon@users.noreply.github.com>
2019-08-04 10:44:21 -0600

    Add code to read input file
    Add code to main() that reads all relevant information from input file, updates string section vectors accordingly

--- main.cpp
+++ main.cpp
@@ -36,10 +36,73 @@ int main(){
 			}
 		inputFile.ignore(256, '\n'); // skip first line of input file
 		while(true){ // until entire file is read
-			string stringRead;
+			string section;
+			int vectorIndex;
+			int masters, pops, specials, kids;
+			string title;
+			string name;
+			inputFile >> section;
+			inputFile >> masters >> pops >> specials >> kids;
+			inputFile >> title;
+			getline(inputFile, name, '\n');
+			if(section == "vln1"){
+				violin1Section.resize(violin1Section.size() + 1); // increase section size by 1
+				vectorIndex = violin1Section.size() - 1; // find last index of section vector
+				violin1Section[vectorIndex].add_masters(masters);
+				violin1Section[vectorIndex].add_pops(pops);
+				violin1Section[vectorIndex].add_specials(specials);
+				violin1Section[vectorIndex].add_kids(kids);
+				violin1Section[vectorIndex].set_title(title);
+				violin1Section[vectorIndex].set_name(name);
+			}
+			else if(section == "vln2"){
+				violin2Section.resize(violin2Section.size() + 1); // increase section size by 1
+				vectorIndex = violin2Section.size() - 1; // find last index of section vector
+				violin2Section[vectorIndex].add_masters(masters);
+				violin2Section[vectorIndex].add_pops(pops);
+				violin2Section[vectorIndex].add_specials(specials);
+				violin2Section[vectorIndex].add_kids(kids);
+				violin2Section[vectorIndex].set_title(title);
+				violin2Section[vectorIndex].set_name(name);
+			}
+			else if(section == "vla"){
+				violaSection.resize(violaSection.size() + 1); // increase section size by 1
+				vectorIndex = violaSection.size() - 1; // find last index of section vector
+				violaSection[vectorIndex].add_masters(masters);
+				violaSection[vectorIndex].add_pops(pops);
+				violaSection[vectorIndex].add_specials(specials);
+				violaSection[vectorIndex].add_kids(kids);
+				violaSection[vectorIndex].set_title(title);
+				violaSection[vectorIndex].set_name(name);
+			}
+			else if(section == "vcl"){
+				celloSection.resize(celloSection.size() + 1); // increase section size by 1
+				vectorIndex = celloSection.size() - 1; // find last index of section vector
+				celloSection[vectorIndex].add_masters(masters);
+				celloSection[vectorIndex].add_pops(pops);
+				celloSection[vectorIndex].add_specials(specials);
+				celloSection[vectorIndex].add_kids(kids);
+				celloSection[vectorIndex].set_title(title);
+				celloSection[vectorIndex].set_name(name);
+			}
+			else if(section == "cb"){
+				bassSection.resize(bassSection.size() + 1); // increase section size by 1
+				vectorIndex = bassSection.size() - 1; // find last index of section vector
+				bassSection[vectorIndex].add_masters(masters);
+				bassSection[vectorIndex].add_pops(pops);
+				bassSection[vectorIndex].add_specials(specials);
+				bassSection[vectorIndex].add_kids(kids);
+				bassSection[vectorIndex].set_title(title);
+				bassSection[vectorIndex].set_name(name);
+			}
+		} // input file read
+		cout << "Roster updated!" << endl;
 
-		}
-		cout << "Ending program.";
-		return 0;
-	}
+		cout << endl << "Please select from the following options:" << endl;
+		cout << "1. Create new program" << endl;
+		cout << "2. View/edit existing program" << endl;
+		cout << "3. View/edit section roster" << endl;
+		cout << "9. Exit program" << endl;
+	} // end of main program loop
+	return 0;
 }
