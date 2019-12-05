87a51863d937d590e212390a20684711f9e1e174
mikeybassoon <mike.macaulay@gmail.com>
2019-12-04 18:02:21 -0600

    Initial function building

--- Node/stringManager.js
+++ Node/stringManager.js
@@ -1,4 +1,36 @@
-let vln1section = {
-	let musicians = [];
-	let size = function(musicians) => {return musicians.
-};
+var fs = Require('fs');
+
+function Musician(){
+	// gigs this musician scheduled for, by series
+	this.popsGigs;
+	this.mwGigs;
+	this.specialsGigs;
+	this.familyGigs;
+}
+
+function Section() {
+	this.musicians = new Array();
+	this.Sort = function(series) {
+		if(series === 'pops')
+			musicians.sort(function(a,b){return a.popsGigs - b.popsGigs});
+		else if(series === 'mw')
+			musicians.sort(function(a,b){retun a.mwGigs - b.mwGigs});
+		else if(series === 'specials')
+			musicians.sort(function(a,b){return a.mwGigs - b. mwGigs});
+		else if(series === 'family')
+			musicians.sort(function(a,b){return a.familyGigs - b.familyGigs});
+		else
+			console.log('Invalid series passed to sort function: ' + series);
+	}
+}
+
+var vln1section = new Section();
+
+let vln2section = vln1section;
+
+let vlaSection = vln1section;
+
+let vclSection = vln1section;
+
+let cbSection = vln1section;
+
